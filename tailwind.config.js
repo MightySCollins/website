11278609be8dbc0829bac2d0b300149c0c546489
Sam Collins <sam@cyberbyte.software>
2025-04-14 18:09:33 +0100

    Updated tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,11 @@
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: [
-    "index.html"
-  ],
+  purge: ["index.html"],
   jit: true,
   darkMode: false, // or 'media' or 'class'
-  theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      white: colors.white,
-      gray: colors.coolGray
-    },
-  },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
